d5950d2175f808a39c0dc62820e100b274650fe0
LienPaul <zinfu1111@gmail.com>
2021-12-14 00:56:39 +0800

    加入coredata

---
+++ PLAccountingBook/RecordManager.swift
@@ -0,0 +1,121 @@
+//
+//  RecordManager.swift
+//  PLAccountingBook
+//
+//  Created by 連振甫 on 2021/12/13.
+//
+
+import UIKit
+import CoreData
+
+struct Record {
+    
+    init(dataMO: RecordMO){
+        self.id = Int(dataMO.id)
+        self.content = dataMO.content ?? ""
+        self.cost = dataMO.cost
+        self.tag = dataMO.tag ?? ""
+        self.datetime = dataMO.datetime ?? Date()
+    }
+    
+    internal init(id: Int, content: String, cost: Double, tag: String, datetime: Date) {
+        self.id = id
+        self.content = content
+        self.cost = cost
+        self.tag = tag
+        self.datetime = datetime
+    }
+    
+    
+    let id:Int
+    var content:String
+    var cost:Double
+    var tag:String
+    var datetime:Date
+    
+    
+    
+}
+
+class RecordManager {
+    
+    static let shared = RecordManager()
+    var appDelegate:AppDelegate!
+    var context:NSManagedObjectContext!
+    
+    init() {
+        appDelegate = UIApplication.shared.delegate as? AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+    }
+    
+    private func newId() -> Int {
+        return query().count
+    }
+    
+    func insert(with data:Record) {
+        
+        let record = RecordMO(context: context)
+        record.id = Int64(newId())
+        record.content = data.content
+        record.cost = data.cost
+        record.tag = data.tag
+        record.datetime = Date()
+        appDelegate.saveContext()
+        
+    }
+    
+    func find(by id:Int) -> Record? {
+        let fetchRequest: NSFetchRequest<RecordMO> = RecordMO.fetchRequest()
+        do {
+            guard let data = try context.fetch(fetchRequest).first(where: {$0.id == id}) else {
+                return nil
+            }
+            
+            return Record(dataMO: data)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    func query() -> [Record] {
+        let fetchRequest: NSFetchRequest<RecordMO> = RecordMO.fetchRequest()
+        do {
+            let data = try context.fetch(fetchRequest)
+            
+            var result = [Record]()
+            data.forEach { item in
+                result.append(Record(dataMO: item))
+            }
+            return result
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    func update(with data:Record) {
+        let fetchRequest: NSFetchRequest<RecordMO> = RecordMO.fetchRequest()
+        do {
+            if let record = try context.fetch(fetchRequest).first(where: { $0.id == data.id }){
+                record.datetime = data.datetime
+                record.tag = data.tag
+                record.cost = data.cost
+                record.content = data.content
+                appDelegate.saveContext()
+            }
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    func delete(with data:Record) {
+        let fetchRequest: NSFetchRequest<RecordMO> = RecordMO.fetchRequest()
+        do {
+            if let record = try context.fetch(fetchRequest).first(where: { $0.id == data.id }){
+                context.delete(record)
+                appDelegate.saveContext()
+            }
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+}
