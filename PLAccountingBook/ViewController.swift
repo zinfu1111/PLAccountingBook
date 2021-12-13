52f28094e9f47b2730ea72508217161acc303455
LienPaul <zinfu1111@gmail.com>
2021-12-13 23:03:59 +0800

    Initial Commit

---
+++ PLAccountingBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PLAccountingBook
+//
+//  Created by 連振甫 on 2021/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
