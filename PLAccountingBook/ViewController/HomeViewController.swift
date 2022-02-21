7b7f39b04cc8fdfe014a179fb050583c32fa121e
Paul <zinfu1111@gmail.com>
2022-02-21 17:58:01 +0800

    出大事了上下滑會死掉

--- PLAccountingBook/ViewController/HomeViewController.swift
+++ PLAccountingBook/ViewController/HomeViewController.swift
@@ -6,18 +6,29 @@
 //
 
 import UIKit
+import FSCalendar
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var createButtonView: UIView!
     @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var totalCostLabel: UILabel!
+    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
     
     private var viewModel: HomeViewModel!
     private var dataSource: RecordTableViewDataSource<RecordCell,Record>!
+    
+    fileprivate lazy var scopeGesture: UIPanGestureRecognizer = {
+        [unowned self] in
+        let panGesture = UIPanGestureRecognizer(target: self.calendar, action: #selector(self.calendar.handleScopeGesture(_:)))
+        panGesture.delegate = self
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 2
+        return panGesture
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +36,19 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         overrideUserInterfaceStyle = .light
         viewModel = HomeViewModel()
+        
+        
+        self.calendar.select(Date())
+        
+        self.view.addGestureRecognizer(self.scopeGesture)
+//        self.tableView.panGestureRecognizer.require(toFail: self.scopeGesture)
+        self.calendar.scope = .week
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         createButtonView.layer.cornerRadius = createButtonView.bounds.height * 0.5
-        viewModel.updateData(date: datePicker.date)
+        viewModel.updateData(date: self.calendar.selectedDate!)
         updateTableView()
         updateLabel()
     }
@@ -51,18 +69,53 @@ class HomeViewController: UIViewController {
     
     @IBAction func goEditRecord(_ sender: Any) {
         let editRecordVC = self.storyboard?.instantiateViewController(withIdentifier: "\(EditRecordViewController.self)") as! EditRecordViewController
-        editRecordVC.viewModel = EditRecordViewModel(record: Record(id: 0, content: "", cost: 0, tag: "", datetime: datePicker.date))
+        editRecordVC.viewModel = EditRecordViewModel(record: Record(id: 0, content: "", cost: 0, tag: "", datetime: self.calendar.selectedDate!))
         navigationController?.pushViewController(editRecordVC, animated: true)
         
+    }
+}
+
+extension HomeViewController : FSCalendarDataSource, FSCalendarDelegate, UIGestureRecognizerDelegate {
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let shouldBegin = self.tableView.contentOffset.y <= -self.tableView.contentInset.top
+        if shouldBegin {
+            let velocity = self.scopeGesture.velocity(in: self.view)
+            switch self.calendar.scope {
+            case .month:
+                return velocity.y < 0
+            case .week:
+                return velocity.y > 0
+            }
+        }
+        return shouldBegin
+    }
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+//        self.calendarHeightConstraint.constant = bounds.height
+//        self.view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+//        print("did select date \(self.dateFormatter.string(from: date))")
+//        let selectedDates = calendar.selectedDates.map({self.dateFormatter.string(from: $0)})
+//        print("selected dates is \(selectedDates)")
+        if monthPosition == .next || monthPosition == .previous {
+            calendar.setCurrentPage(date, animated: true)
+        }
+    }
+
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+//        print("\(self.dateFormatter.string(from: calendar.currentPage))")
     }
 }
 
 extension HomeViewController {
     
     private func updateLabel(){
-        self.dateLabel.text = datePicker.date.toString(format: "YYYY年M月d日")
+        self.dateLabel.text = self.calendar.selectedDate!.toString(format: "YYYY年M月d日")
         self.totalCostLabel.text =
-            self.viewModel.recordData.filter({$0.datetime.toString(format: "YYYY/MM/dd") == datePicker.date.toString(format: "YYYY/MM/dd")}).map({ $0.cost }).reduce(0, {$0 + $1}).toMoneyFormatter()
+            self.viewModel.recordData.filter({$0.datetime.toString(format: "YYYY/MM/dd") == self.calendar.selectedDate!.toString(format: "YYYY/MM/dd")}).map({ $0.cost }).reduce(0, {$0 + $1}).toMoneyFormatter()
     }
     
     private func updateTableView() {
